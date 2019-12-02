ea871697e65d7f6c7aa49d6b870fd5fa6951b71c
Richie Preece <preece@adobe.com>
2019-12-02 14:15:42 -0700

    Include build

---
+++ build/precache-manifest.05ab52d00d9ab68d273422490b9420be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ba74a5e7dddeb123c87c56a480e170b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52a816fdf1e34d12bb0c",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "8b05f984efde3f6f190b",
+    "url": "/static/js/2.61fcc5ef.chunk.js"
+  },
+  {
+    "revision": "52a816fdf1e34d12bb0c",
+    "url": "/static/js/main.e13e8496.chunk.js"
+  },
+  {
+    "revision": "827dd3240a85be9f97fa",
+    "url": "/static/js/runtime-main.d47b68e4.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
